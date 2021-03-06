5a84e0f46f4bc50f2da15ea286447e50151a8e00
Wayne <wabbruscatoorr@gmail.com>
2021-03-05 21:51:41 -0500

    Updating test file with data array- 3 objects

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import requests
 
 BASE = "http://127.0.01:5000/"  # LocalHost url that this is running on
+data = [
+    {"likes": 18978013, "name": "Conan the Barbarian", "views": 1789007},
+    {"likes": 954475, "name": "Waiting...", "views": 86225634},
+    {"likes": 165152, "name": "The Game", "views": 14923008746}
+]
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "Wayne", "views": 1853205})
-print(response.json())
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
 
 input()
+response = requests.delete(BASE + "video/0")
+print(response.json())
+input()
 
-response = requests.get(BASE + "video/1")
+response = requests.get(BASE + "video/2")
 print(response.json())
