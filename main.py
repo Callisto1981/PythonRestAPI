6505caade57c8e749c0bd5f0fe006b2268e5a3ac
Wayne <wabbruscatoorr@gmail.com>
2021-03-05 20:29:00 -0500

    updating main with names object and resource endpoint

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ app = Flask(__name__)
 api = Api(app)
 
 
+names = {
+    "wayne": {"age": 39, "gender": "male"},
+    "mariam": {"age": 26, "gender": "female"}
+}
+
+
 class HelloWorld (Resource):
-    def get(self):
-        return {"data": "Hello Wordld"}
+    def get(self, name, test):
+        return {"name": name, "test": test}
 
 
-api.add_resource(HelloWorld, "/helloWordl")
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
 
 if __name__ == "__main__":
-    app.run(debug=True) # Only run in Development environment to test
+    app.run(debug=True)  # Only run in Development environment to test
