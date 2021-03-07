91cbacb420e23f7c43eb14d93aa11d9c8c954a7b
Wayne <wabbruscatoorr@gmail.com>
2021-03-06 20:39:21 -0500

    Updating model with repr function

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)  # Max characters for youtube video 'this is a youtube api clone'
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self, name, views, likes):
+        return f"Video(name={name}, views={views}, likes={likes})"
+    # Need to fix f string
+
 db.create_all()
 
 video_put_args = reqparse.RequestParser()
