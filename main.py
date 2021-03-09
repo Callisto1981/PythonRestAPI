5b2f597c3a9666668a14e8e98d5f16ad8a6b516d
Wayne <wabbruscatoorr@gmail.com>
2021-03-09 18:07:40 -0500

    Added patchrequest and fixed code

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ class VideoModel(db.Model):
     likes = db.Column(db.Integer, nullable=False)
 
     def __repr__(self, name, views, likes):
-        return f"Video(name={name}, views={views}, likes={likes})"
-    # Need to fix f string
+        return format("Video(name={name}, views={views}, likes={likes})")
 
 
 db.create_all()
@@ -26,6 +25,10 @@ video_put_args.add_argument("name", type=str, help="Name of the video", required
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
 
 resource_fields = {
     'id': fields.Integer,
@@ -39,19 +42,41 @@ class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
         result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message= "Could not find video with that id")
         return result
 
     @marshal_with(resource_fields)
     def put(self, video_id):
         args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, nessage="Video id taken...")
         video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args["likes"])
         db.session.add(video)
         db.session.commit()
         return video, 201
 
-    def delete(self,  video_id):
-        # abort_if_video_id_doesnt_exist(video_id)
-        # del videos[ video_id]
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
+
+        if args["name"]:
+            result.name = args["name"]
+        if args["views"]:
+            result.views = args["views"]
+        if args["likes"]:
+            result.likes = args["likes"]
+
+        db.session.commit()
+
+        return result
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[ video_id]
         return '', 204
 
 
--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 import requests
 
 BASE = "http://127.0.01:5000/"  # LocalHost url that this is running on
-data = [
-    {"likes": 18978013, "name": "Conan the Barbarian", "views": 1789007},
-    {"likes": 954475, "name": "Waiting...", "views": 86225634},
-    {"likes": 165152, "name": "The Game", "views": 14923008746}
-]
+#data = [
+#    {"likes": 18978013, "name": "Conan the Barbarian", "views": 1789007},
+#    {"likes": 954475, "name": "Waiting...", "views": 86225634},
+#   {"likes": 165152, "name": "The Game", "views": 14923008746}
+#]
 
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+#for i in range(len(data)):
+#    response = requests.put(BASE + "video/" + str(i), data[i])
+#    print(response.json())
+
+#input()
+#response = requests.get(BASE + "video/2")
 
 
-input()
-response = requests.get(BASE + "video/2")
+response = requests.patch(BASE + "video/2", {"views":99})
 print(response.json())
